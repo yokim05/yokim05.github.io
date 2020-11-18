c12edfe8df2f0701f9c28c2664d813d0319f29d5
dhika roid <dhikayokim@gmail.com>
2020-11-18 14:51:27 +0700

    init

---
+++ content/template/footer.php
@@ -0,0 +1,24 @@
+<footer class="footer-section">
+  <div class="container">
+    <div class="row">
+      <div class="col-xl-6 col-lg-5 order-lg-1">
+        <img src="img/logo.png" alt="" style="width: 30%;">
+        <div class="copyright">
+          <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+          Copyright &copy;<script>
+            document.write(new Date().getFullYear());
+          </script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+          <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+        </div>
+        <p>Polish by Yokim with <i class="fa fa-heart-o" aria-hidden="true"></i> In Subang</p>
+        <div class="social-links">
+          <a href=""><i class="fa fa-instagram"></i></a>
+          <a href=""><i class="fa fa-pinterest"></i></a>
+          <a href=""><i class="fa fa-facebook"></i></a>
+          <a href=""><i class="fa fa-twitter"></i></a>
+          <a href=""><i class="fa fa-youtube"></i></a>
+        </div>
+      </div>
+    </div>
+  </div>
+</footer>
---
+++ content/template/header.php
@@ -0,0 +1,18 @@
+<header class="header-section clearfix">
+  <a href="index.php" class="site-logo">
+    <img src="img/logo.png" alt="" style="width: 45%;">
+  </a>
+  <ul class="main-menu">
+    <li><a href="index.php">Home</a></li>
+    <li><a href="#">About</a></li>
+    <li><a href="#">Pages</a>
+      <ul class="sub-menu">
+        <li><a href="#">HTML-5</a></li>
+        <li><a href="#">CSS-3</a></li>
+        <li><a href="#">PHP</a></li>
+        <li><a href="#">Javascript</a></li>
+      </ul>
+    </li>
+    <li><a href="contact.php">Contact</a></li>
+  </ul>
+</header>
